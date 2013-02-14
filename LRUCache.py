ec24af24009dbb01a782c33655f5958d6f502064
Varun Shah <varun_shah@yahoo.com>
2013-02-13 21:24:45 -0800

    Added reading from standard input logic

--- LRUCache.py
+++ LRUCache.py
@@ -1,7 +1,7 @@
 import collections
 
-class LastUpdatedOrderedDict(collections.OrderedDict):
-	"""docstring for LastUpdatedOrderedDict"""
+class LRUCache(collections.OrderedDict):
+	"""LRUCache adds methods to OrderedDict to make it work like a LRU ordered dict with a bound"""
 
 	def __setitem__(self, key, value):
 		if key in self:
@@ -10,7 +10,7 @@ class LastUpdatedOrderedDict(collections.OrderedDict):
 			self.popitem(False)
 		collections.OrderedDict.__setitem__(self, key, value)
 
-	def setbound(self, bound):
+	def setbound(self, bound=1000000):
 		self.bound = bound
 
 	def peek(self, key):
@@ -33,33 +33,28 @@ class LastUpdatedOrderedDict(collections.OrderedDict):
 		for key in sorted(self.iterkeys()):
 			print key, self[key]
 
-# number_of_lines = int(raw_input())
-# bound = raw_input().split()
-# bound = int(bound[1])
-# print bound
-# a = []
-# b = LRUCache(bound)
-# for i in range(1, number_of_lines):
-# 	a.append(raw_input())
-# for i in a:
-# 	temp = i.split()
-# 	if temp[0] == "SET":
-# 		b.set()
+# Start Reading standard input
+number_of_lines = int(raw_input())
+bound = raw_input().split()
+bound = int(bound[1])
 
-# print a
-#a = LRUCache(bound)
+# Instantiate new list to hold standard input and LRU cache object
+a = []
+d1 = LRUCache()
+d1.setbound(bound)
 
-d1 = LastUpdatedOrderedDict()
-d1.setbound(4)
-d1['a'] = 'A'
-d1['b'] = 'B'
-d1['c'] = 'C'
-d1['d'] = 'D'
-d1['e'] = 'E'
-d1['a'] = 'N'
-d1.get('b')
-d1.peek('c')
-d1.get('e')
-print "====================================================================="
-d1.dump()
-print "====================================================================="
+# Read remaining lines of standard input
+for i in range(1, number_of_lines):
+	a.append(raw_input())
+
+# For each line, perform one of the operations.
+for i in a:
+	temp = i.split()
+	if temp[0] == "SET":
+		d1[temp[1]] = temp[2]
+	elif temp[0] == "GET":
+		d1.get(temp[1])
+	elif temp[0] == "DUMP":
+		d1.dump()
+	elif temp[0] == "PEEK":
+		d1.peek(temp[1])
